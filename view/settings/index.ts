7a8a520098e2e297816662bb1f49add794531eee
InkerBot <im@inker.bot>
2025-10-17 04:32:27 -0300

    remove unused element

--- view/settings/index.ts
+++ view/settings/index.ts
@@ -1,23 +1,11 @@
-import type { WritableAtom } from 'nanostores'
-
 import {
   show3d,
   showCharts,
   showP3,
   showRec2020
 } from '../../stores/settings.ts'
-import { getCheckbox, onChange } from '../checkbox/index.ts'
 
-function init(store: WritableAtom<boolean>, checkbox: HTMLInputElement): void {
-  store.subscribe(show => {
-    checkbox.checked = show
-  })
-  onChange(checkbox, checked => {
-    store.set(checked)
-  })
-}
-
-init(showCharts, getCheckbox('charts')!)
-init(showP3, getCheckbox('p3')!)
-init(showRec2020, getCheckbox('rec2020')!)
-init(show3d, getCheckbox('mode3d')!)
+showCharts.set(true)
+showP3.set(false)
+showRec2020.set(false)
+show3d.set(true)
